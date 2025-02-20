56749bbe9a9aa82c5cdecb3680ec5c7cbe878bb1
AlfaHachiman <alfaaora11@gmail.com>
2025-02-20 22:31:24 +0700

    Update fitur baru

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-//let nama = prompt('Selamat datang! inputkan nama kamu ya.')
-   // alert('nama kamu adalah ' + nama)
-
-// Memastikan musik diputar saat halaman dimuat
 document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById('background-music');
     audio.play().catch(function(error) {
         console.log("Autoplay diblokir oleh browser. Silakan interaksi dengan halaman untuk memutar musik.");
     });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".kolomcari button").addEventListener("click", searchFunction);
+
+    function searchFunction() {
+        let keyword = document.querySelector('.search-input').value.toLowerCase();
+        let sections = {
+            "home": "home-section",
+            "produk": "produk-section",
+            "kontak": "kontak-section",
+            "tentang": "tentang-section"
+        };
+
+        if (sections[keyword]) {
+            document.getElementById(sections[keyword]).scrollIntoView({ behavior: "smooth" });
+        } else {
+            alert("Fitur ini masih coming soon guys...");
+        }
+    }
 });
